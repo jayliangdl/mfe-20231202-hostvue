d0483858124883d196df6f1c1399007d485c87c4
CN087005 <jay.liang@amway.com>
2023-12-03 15:44:58 +0800

    一系列改动

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -10,6 +10,6 @@ module.exports = {
   /**应用自己的配置放在这里*/
   appConfig:{
     reactConsumURL:"https://mfe-20231202-remote.vercel.app",  /**注意是http://xxx:port  不是 http://xxx:port/  最后是没有 / 的 */
-    vueConsumURL:"https://mfe-20231202-remote-vue.vercel.app",  /**注意是http://xxx:port  不是 http://xxx:port/  最后是没有 / 的 */
+    vueConsumURL:"https://mfe-20231202-remotevue.vercel.app",  /**注意是http://xxx:port  不是 http://xxx:port/  最后是没有 / 的 */
   }
 }
